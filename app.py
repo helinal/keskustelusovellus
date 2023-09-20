fd9e4a0286ae6429311df4be9b1c56a06f97a6fc
Helinä Lähteenmäki <helina.lahteenmaki@helsinki.fi>
2023-09-20 10:06:14 +0300

    Add the basic core of a logging in functionality

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 from os import getenv
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.secret_key = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 
 @app.route("/")
@@ -24,4 +25,17 @@ def send():
     sql = text("INSERT INTO messages (content) VALUES (:content)")
     db.session.execute(sql, {"content":content})
     db.session.commit()
+    return redirect("/")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    # TODO: check username and password
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
     return redirect("/")
